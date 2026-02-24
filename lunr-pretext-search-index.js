822d248c81627c63a2a009bcfb94f265f0220ac7
Randall Helmstutler <rhelmstu@umw.edu>
2026-02-24 15:00:16 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -15,35 +15,80 @@ var ptx_lunr_docs = [
   "url": "section-cpsc.html",
   "type": "Section",
   "number": "1",
-  "title": "Computer Science &amp; Cybersecurity",
-  "body": " Computer Science & Cybersecurity     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  Where    2:00-2:45: Opportunities Beyond the Classroom  Where    2:45-3:15: Meet the Faculty  Where    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Where    4:00-4:30: Pick-up and Day Eve Celebration  Where     "
+  "title": "Computer Science",
+  "body": " Computer Science     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  CPSC 326: Theory of Computing  Dr. Andrew Marshall  Farmer 138    2:00-2:45: Opportunities Beyond the Classroom: Undergraduate Research Experiences  Farmer 036    2:45-3:15: Meet the Faculty  Person  Where    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Farmer 036    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
 },
 {
-  "id": "section-1",
+  "id": "section-cyber",
   "level": "1",
-  "url": "section-1.html",
+  "url": "section-cyber.html",
   "type": "Section",
   "number": "2",
-  "title": "Mathematics",
-  "body": " Mathematics     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  Where    2:00-2:45: Opportunities Beyond the Classroom  Where    2:45-3:15: Meet the Faculty  Where    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Where    4:00-4:30: Pick-up and Day Eve Celebration  Where     "
+  "title": "Cybersecurity",
+  "body": " Cybersecurity     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  MATH 253: Introduction to Cryptography  Prof. Jennifer Magee  Farmer 036    2:00-2:45: Opportunities Beyond the Classroom: Undergraduate Research Experiences  Farmer 036    2:45-3:15: Meet the Faculty  Person  Where    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Farmer 036    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
+},
+{
+  "id": "section-data",
+  "level": "1",
+  "url": "section-data.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Data Science &amp; Statistics",
+  "body": " Data Science & Statistics     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  STAT 280: Intermediate Statistics  Dr. Debra Hydorn  Farmer 140    2:00-2:45: Opportunities Beyond the Classroom: Undergraduate Research Experiences  Farmer 036    2:45-3:15: Meet the Faculty  Dr. Melody Denhere  Farmer 128    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Farmer 036    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
+},
+{
+  "id": "section-econ",
+  "level": "1",
+  "url": "section-econ.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Economics",
+  "body": " Economics     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  ECON 300: Introduction to Economic Analysis  Dr. Brad Hansen  Monroe 122    2:00-2:45: Opportunities Beyond the Classroom  Class visit with the Student-managed Investment Fund (FINC 412)  Dr. Rachel Graefe-Anderson  Woodard 202    2:45-3:15: Meet the Faculty  Dr. Brad Hansen  Woodard 238    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Woodard 202    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
+},
+{
+  "id": "section-finc",
+  "level": "1",
+  "url": "section-finc.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Finance &amp; Accounting",
+  "body": " Finance & Accounting     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Meet with Dr. Rachel Graefe-Anderson and students  Woodard 249    2:00-2:45: Opportunities Beyond the Classroom  Class visit with the Student-managed Investment Fund (FINC 412)  Dr. Rachel Graefe-Anderson  Woodard 202    2:45-3:15: Meet the Faculty  Dr. Ken Machande (accounting)  Woodard 241  Dr. Rachel Graefe-Anderson (finance)  Woodard 237    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Woodard 202    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
+},
+{
+  "id": "section-mang",
+  "level": "1",
+  "url": "section-mang.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Management &amp; Business Administration",
+  "body": " Management & Business Administration     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  MGMT 490: Strategic Management  Dr. Samira Fallah  Woodard 132    2:00-2:45: Opportunities Beyond the Classroom: Pitch and Case Competitions  Woodard 132    2:45-3:15: Meet the Faculty  Drs. Mukesh Srivastava and Chris Willis  Woodard 240  MBA Program with Dr. Chris Garcia  Woodard 214    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Woodard 202    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
 },
 {
   "id": "section-2",
   "level": "1",
   "url": "section-2.html",
   "type": "Section",
-  "number": "3",
+  "number": "7",
   "title": "Marketing",
-  "body": " Marketing     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  Where    2:00-2:45: Opportunities Beyond the Classroom  Where    2:45-3:15: Meet the Faculty  Where    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Where    4:00-4:30: Pick-up and Day Eve Celebration  Where     "
+  "body": " Marketing     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  MKTG 411: Marketing Research  Dr. Kashef Majid  Woodard 211    2:00-2:45: Opportunities Beyond the Classroom: Pitch and Case Competitions  Woodard 132    2:45-3:15: Meet the Faculty  Dr. Lance Gentry  Woodard 257  MBA Program with Dr. Chris Garcia  Woodard 214    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Woodard 202    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
+},
+{
+  "id": "section-1",
+  "level": "1",
+  "url": "section-1.html",
+  "type": "Section",
+  "number": "8",
+  "title": "Mathematics",
+  "body": " Mathematics     12:00-12:45: Lunch  Woodard Red Room    1:00-1:50: Class visits  MATH 253: Introduction to Cryptography  Prof. Jennifer Magee  Farmer 036    2:00-2:45: Opportunities Beyond the Classroom: Undergraduate Research Experiences  Farmer 036    2:45-3:15: Meet the Faculty  Dr. Randall Helmstutler (cryptography)  Farmer 122  Dr. Larry Lehman (number theory)  Farmer 130  Dr. Keith Nabb (mathematics education)  Farmer 129    3:15-4:00: Student Panel: Getting Involved with Clubs, Internships, and More  Farmer 036    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
 },
 {
   "id": "section-3",
   "level": "1",
   "url": "section-3.html",
   "type": "Section",
-  "number": "4",
+  "number": "9",
   "title": "Parents and Supporters",
-  "body": " Parents and Supporters     3:15-4:00: Q & A with Dr. Randall Helmstutler,  Director of Undergraduate Programs  Woodard 249    4:00-4:30: Pick-up and Day Eve Celebration  Where     "
+  "body": " Parents and Supporters     3:15-4:00: Q & A with Dr. Randall Helmstutler,  Director of Undergraduate Programs  Woodard 249    4:00-4:30: Pick-up and Day Eve Celebration  Woodard Red Room     "
 }
 ]
 
